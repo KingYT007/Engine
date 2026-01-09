7df1166a1215bce300c0033291b7a9a32fac09c3
KingYT007 <sh86ampano@gmail.com>
2026-01-09 11:42:27 +0600

    Add initialization OpenGL and GLFW

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,27 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <cmath>
+
+
+
+int windowX = 1980;
+int windowY = 1080;
+
+
+void windowSizeCallback(GLFWwindow* window, int width, int height)
+{
+    windowX = width;
+    windowY = height;
+    glViewport(0, 0, width, height);
+}
+
+void glfwKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
 
 int main(void)
 {
@@ -10,8 +31,11 @@ int main(void)
     if (!glfwInit())
         return -1;
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(windowX, windowY, "Hello World", nullptr, nullptr);
     if (!window)
     {
         glfwTerminate();
@@ -21,18 +45,24 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    glfwSetWindowSizeCallback(window, windowSizeCallback);
+    glfwSetKeyCallback(window, glfwKeyCallback);
+
     if(!gladLoadGL())
     {
-        std::cout << "Failed to initialize GLAD" << std::endl;
+        std::cerr << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
-    std::cout << "OpenGL " << GLVersion.major << "." << GLVersion.minor << std::endl;
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "Version: " << glGetString(GL_VERSION) << std::endl;
 
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
+        int i = 0;
+        glClearColor(fmod(i/3, 1.0), 0.3f, 0.3f, 1.0f);
+        i++;
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
